858355e272979bcf615b710fd7ab231e7b36b479
Tenaity <tenaiti.150320@gmail.com>
2021-09-18 11:50:42 +0700

    init

---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const User = require("../models/User")
+
+//REGISTER
+router.post("/register", async(req,res)=>{
+    const {username, email, password} = req.body
+    try{
+        const newUser = new User({
+            username,
+            email,
+            password,
+        })
+        const user = await newUser.save();
+        res.status(200).json(user);
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+//LOGIN
+module.exports = router
